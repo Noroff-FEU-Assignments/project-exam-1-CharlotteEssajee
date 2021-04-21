05172a228d25b0c15c33c6ec32af8728434584ba
CharlotteEssajee <71342246+CharlotteEssajee@users.noreply.github.com>
2021-04-21 22:09:51 +0200

    pages and contact form
    setting up the different pages, and making the contact form

---
+++ js/script.js
@@ -0,0 +1,62 @@
+/* navbar toggle */
+const toggleButton = document.getElementsByClassName("toggleButton")[0];
+const navbarLinks = document.getElementsByClassName("navbarLinks")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+/* contactForm */
+const form = document.querySelector("#contactForm");
+const fullname = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (checkLength(fullname.value, 4) === true) {
+    fullNameError.style.display = "none";
+  } else {
+    fullNameError.style.display = "block";
+  }
+
+  if (checkLength(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(subject.value, 15) === true) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (checkLength(message.value, 24) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
